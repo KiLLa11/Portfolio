d995b91030bf7765316469e027bed875edbcb94e
KiLLa11 <makviser24@gmail.com>
2022-10-25 01:37:18 +0600

    page add

--- mode-switch.js
+++ mode-switch.js
@@ -1,10 +1,14 @@
 var mode_btn = document.querySelector(".main-footer__btn");
+var light = "Light";
+var dark = "Dark";
 var element = document.body;
 mode_btn.addEventListener("click", () => {
     if (element.classList.contains("dark-mode")) {
         element.classList.remove("dark-mode");
+        mode_btn.innerHTML = light;
     } else {
         element.classList.add("dark-mode");
+        mode_btn.innerHTML = dark;
     }
 
 })
